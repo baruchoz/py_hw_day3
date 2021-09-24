c4e67d507322a4e02fb9dea62acdcd4fba228ee2
Baruch Rotbart <BaruchRotbart@WesleyHills-MacBook-Pro.local>
2021-09-24 15:00:19 -0500

    hw edits

---
+++ module_homework.py
@@ -0,0 +1,11 @@
+from math import pi
+
+def square_footage(length,width):
+    return length * width
+
+def circumference(radius):
+    return 2 * pi * radius
+
+
+print(square_footage(4,8))
+print(circumference(7))
